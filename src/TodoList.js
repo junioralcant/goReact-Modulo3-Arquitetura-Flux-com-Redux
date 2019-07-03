6eeab791fcd5d3438f047fe2667593a975d2fcf2
Junior Marques <junioralcant111@gmail.com>
2019-07-03 14:31:43 -0300

    06 Remover todo

--- src/TodoList.js
+++ src/TodoList.js
@@ -9,11 +9,14 @@ import * as TodoActions from "./store/actions/todo";
 import { connect } from "react-redux";
 
 // Lista os todos
-const TodoList = ({ todos, addTodo }) => (
+const TodoList = ({ todos, addTodo, removeTodo }) => (
   <Fragment>
     <ul>
       {todos.map(todo => (
-        <li key={todo.id}>{todo.text}</li>
+        <li key={todo.id}>
+          {todo.text}
+          <button onClick={() => removeTodo(todo.id)}>Remover</button>
+        </li>
       ))}
     </ul>
     <button onClick={() => addTodo("Novo todo")}>Adicionar</button>
@@ -22,6 +25,7 @@ const TodoList = ({ todos, addTodo }) => (
 
 TodoList.protoTypes = {
   addTodo: PropTypes.func.isRequired,
+  removeTodo: PropTypes.func.isRequired,
   todos: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.number,
--- src/store/reducers/todos.js
+++ src/store/reducers/todos.js
@@ -8,6 +8,8 @@ export default function todos(state = INITAL_STATE, action) {
     // Ad um todo
     case "ADD_TODO":
       return [...state, { id: Math.random(), text: action.payload.text }]; // Math.random() gera um id aletatório
+    case "REMOVE_TODO":
+      return state.filter(todo => todo.id !== action.payload.id);
     default:
       return state;
   }
