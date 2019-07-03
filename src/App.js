f6de64cdb4fa36e3a723ebd2cd55efe60d355b24
Junior Marques <junioralcant111@gmail.com>
2019-07-03 15:42:29 -0300

    07 Incrementando o reactotron na aplicacao

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+import "./config/ReactotronConfig";
+
 import { Provider } from "react-redux";
 
 import store from "./store";
 
 import Todos from "./TodoList";
+
+console.tron.log("Testando");
 
 const App = () => (
   <Provider store={store}>
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,16 @@
-import { createStore } from "redux";
+import { createStore, compose, applyMiddleware } from "redux";
 
 import reducers from "./reducers";
 
-const store = createStore(reducers);
+// Para poder verificar qualquer alteração feita na aplicação atravez do reactotron
+const composer =
+  process.env.NODE_ENV === "development"
+    ? compose(
+        applyMiddleware(...[]),
+        console.tron.createEnhancer()
+      )
+    : applyMiddleware(...[]);
+
+const store = createStore(reducers, composer);
 
 export default store;
